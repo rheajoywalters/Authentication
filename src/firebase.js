b1801685bd16d0631b5b9647fe26ec2188b553f0
rheajoywalters <54914771+rheajoywalters@users.noreply.github.com>
2019-09-04 15:30:32 -0600

    Update firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const config = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
+  apiKey: "AIzaSyDnT749pto7K9C5N6VvS0aTVvFMJq6QSzI",
+  authDomain: "authentication-d0e33.firebaseapp.com",
+  databaseURL: "https://authentication-d0e33.firebaseio.com",
+  projectId: "authentication-d0e33",
   storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  messagingSenderId: "310675757578",
+  appId: "1:310675757578:web:9e17f766afbce2075bddf3"
 };
 
 firebase.initializeApp(config);
